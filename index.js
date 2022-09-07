c1dc2aed8dd2f73f63312b7a1dd57ccdaed3c0c3
MersedGrbic <101355423+MersedGrbic@users.noreply.github.com>
2022-09-07 21:11:44 +0200

    Added movie section and css styling .

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-// fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=d09ad0f3&s=${inputValue}`)
-// .then(res=>res.json())
-// .then(data=>{
-//    console.log(data)
+
+
+//Function  handleSearch() handles the search button when clicked.
+// It takes the value from input later used as api endpoint in function handleApiCall() that is called inside the function handleSearch().
+
+function handleSearch(){
+   let inputValue = document.getElementById('input-value').value
+   handleApiCall(inputValue);
+    
+}
+
+//Function to get data from api call then use that data as parameter in function handleHTML().
+function handleApiCall(endpoint){
+fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=d09ad0f3&s=${endpoint}`)
+.then(res=>res.json())
+ .then(data=>{
+   console.log(data)
+})
+}
+
+document.getElementById('btn-search').addEventListener('click', handleSearch)
